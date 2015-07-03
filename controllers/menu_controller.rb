dd07b48948e6e5c6106821d6beb47f3d37d4dc08
joelramsey <joel.d.ramsey@gmail.com>
2015-07-03 03:20:49 +0000

    completed command line menu assignment

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -11,10 +11,11 @@ class MenuController
   
     puts "Main Menu - #{@address_book.entries.count} entries"
     puts "1 - View all entries"
-    puts "2 - Create an entry"
-    puts "3 - Search for an entry"
-    puts "4 - Import entries from a CSV"
-    puts "5 - Exit"
+    puts "2 - View Entry Number n"
+    puts "3 - Create an entry"
+    puts "4 - Search for an entry"
+    puts "5 - Import entries from a CSV"
+    puts "6 - Exit"
     print "Enter your selection"
   
     selection = gets.to_i
@@ -25,22 +26,26 @@ class MenuController
        view_all_entries
        main_menu
      when 2
+      system "clear"
+      view_entry_number
+      main_menu
+     when 3
        system "clear"
        create_entry
        main_menu
-     when 3
+     when 4
        system "clear"
        search_entries
        main_menu
-     when 4
+     when 5
        system "clear"
        read_csv
        main_menu
-     when 5
+     when 6
        puts "Good-bye!"
- # #8
+
        exit(0)
- # #9
+ 
      else
        system "clear"
        puts "Sorry, that is not a valid input"
@@ -48,23 +53,35 @@ class MenuController
      end
    end
  
- # #10
+ 
    def view_all_entries
      @address_book.entries.each do |entry|
      system "clear"
      puts entry.to_s
- # #15
+ 
        entry_submenu(entry)
      end
  
      system "clear"
      puts "End of entries"
    end
+  
+   def view_entry_number 
+    system "clear"
+    print "Enter the entry number to view: "
+    selection = gets.to_i
+     if selection > 0
+       puts @address_book.entries[selection-1].to_s
+     else 
+       puts "Invalid Entry.Please enter a valid number"
+     end
+   end
+
  
    def create_entry
      system "clear"
      puts "New AddressBloc Entry"
- # #12
+
      print "Name: "
      name = gets.chomp
      print "Phone number: "
@@ -72,7 +89,7 @@ class MenuController
      print "Email: "
      email = gets.chomp
  
- # #13
+ 
      @address_book.add_entry(name, phone, email)
  
      system "clear"
@@ -86,22 +103,20 @@ class MenuController
    end
   
    def entry_submenu(entry)
- # #16
+
      puts "n - next entry"
      puts "d - delete entry"
      puts "e - edit this entry"
      puts "m - return to main menu"
  
- # #17
+
      selection = gets.chomp
  
      case selection
- # #18
+ 
      when "n"
- # #19
      when "d"
      when "e"
- # #20
      when "m"
        system "clear"
        main_menu
